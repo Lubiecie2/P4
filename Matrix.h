6e90c13b88f9eea27ecb4416ae59af962916e606
Kamil <kgruca7747@gmail.com>
2024-11-27 16:30:51 +0100

    Dodanie metod

--- Matrix.h
+++ Matrix.h
@@ -12,4 +12,8 @@ public:
     Matrix(int n, int* t);            // <--- konstruktor z tablic¹
     Matrix(const Matrix& m);          // <--- konstruktor kopiuj¹cy
     ~Matrix();                        // <--- Destruktor 
+
+
+    Matrix& Macierz_Alokacja(int n);
+    Matrix& Macierz_Odwroc();
 };
